2e10871e442a7b8fbb75e35eada2f6ab2eed970d
Mario Bergeron <AlbertaBeef@users.noreply.github.com>
2021-01-05 12:30:29 -0500

    Update dataset (kaggle), and update scripts accordingly (label 0 is now allocated for non-playing cards, add new decks 08/09/10)

--- dobble_dataset_overview.py
+++ dobble_dataset_overview.py
@@ -16,7 +16,6 @@ import numpy as np
 import os
 import gc
 
-
 #
 # Parameters
 #
@@ -30,9 +29,23 @@ ncols = 146
 
 nchannels = 3
 
+card_decks = [
+    'dobble_deck01_cards_57',
+    'dobble_deck02_cards_55',
+    'dobble_deck03_cards_55',
+    'dobble_deck04_cards_55',
+    'dobble_deck05_cards_55',
+    'dobble_deck06_cards_55',
+    'dobble_deck07_cards_55',
+    'dobble_deck08_cards_55',
+    'dobble_deck09_cards_55',
+    'dobble_deck10_cards_55'
+    ]
+nb_card_decks = len(card_decks)
 print("")
 print("PARAMETERS:")
 print("Normalized shape of images :", ncols, " x ", nrows )
+print("Card Decks : ", nb_card_decks, card_decks)
 
 #
 # Capture images/labels from data set for training and testing
@@ -40,29 +53,16 @@ print("Normalized shape of images :", ncols, " x ", nrows )
 
 def capture_card_filenames(directory_name):
     subdirs = ['{}/{}'.format(directory_name,i) for i in os.listdir(directory_name) ]
-    #labels = os.listdir(directory_name)
     cards = []
     for i,subdir in enumerate(subdirs):
         cards += ['{}/{}'.format(subdir,i) for i in os.listdir(subdir)]
-    #random.shuffle(cards)
     del subdirs
     return cards
 
-train1_dir = dir+'/dobble_deck01_cards_57'
-train2_dir = dir+'/dobble_deck02_cards_55'
-train3_dir = dir+'/dobble_deck03_cards_55'
-train4_dir = dir+'/dobble_deck04_cards_55'
-train5_dir = dir+'/dobble_deck05_cards_55'
-train6_dir = dir+'/dobble_deck06_cards_55'
-train7_dir = dir+'/dobble_deck07_cards_55'
-
-train1_cards = capture_card_filenames(train1_dir)
-train2_cards = capture_card_filenames(train2_dir)
-train3_cards = capture_card_filenames(train3_dir)
-train4_cards = capture_card_filenames(train4_dir)
-train5_cards = capture_card_filenames(train5_dir)
-train6_cards = capture_card_filenames(train6_dir)
-train7_cards = capture_card_filenames(train7_dir)
+train_cards = []
+for d in range(0,nb_card_decks):
+    train_dir = dir+'/'+card_decks[d]
+    train_cards.append( capture_card_filenames(train_dir) )
 
 
 #
@@ -79,56 +79,39 @@ def read_and_process_image(list_of_images):
         y.append(int(y_str[len(y_str)-2]))
     return X,y
 
-train1_X,train1_y = read_and_process_image(train1_cards)
-train2_X,train2_y = read_and_process_image(train2_cards)
-train3_X,train3_y = read_and_process_image(train3_cards)
-train4_X,train4_y = read_and_process_image(train4_cards)
-train5_X,train5_y = read_and_process_image(train5_cards)
-train6_X,train6_y = read_and_process_image(train6_cards)
-train7_X,train7_y = read_and_process_image(train7_cards)
+train_X = []
+train_y = []
+for d in range(0,nb_card_decks):
+   X,y = read_and_process_image(train_cards[d])
+   train_X.append( np.array(X) )
+   train_y.append( np.array(y) )
 
-train1_X = np.array(train1_X)
-train1_y = np.array(train1_y)
-train2_X = np.array(train2_X)
-train2_y = np.array(train2_y)
-train3_X = np.array(train3_X)
-train3_y = np.array(train3_y)
-train4_X = np.array(train4_X)
-train4_y = np.array(train4_y)
-train5_X = np.array(train5_X)
-train5_y = np.array(train5_y)
-train6_X = np.array(train6_X)
-train6_y = np.array(train6_y)
-train7_X = np.array(train7_X)
-train7_y = np.array(train7_y)
 
 print("")
 print("TRAINING DATA SET:")
-print("Shape of deck01 data (X) is :", train1_X.shape)
-print("Shape of deck02 data (X) is :", train2_X.shape)
-print("Shape of deck03 data (X) is :", train3_X.shape)
-print("Shape of deck04 data (X) is :", train4_X.shape)
-print("Shape of deck05 data (X) is :", train5_X.shape)
-print("Shape of deck06 data (X) is :", train6_X.shape)
-print("Shape of deck07 data (X) is :", train7_X.shape)
+for d in range(0,nb_card_decks):
+    print("Shape of deck%02d data (X) is :"%(d+1), train_X[d].shape)
 
 #
 # Display card decks (cards 1-55)
 #
 
-def create_collage(title,cards_X,cards_y):
+def create_collage(deck_id,cards_X,cards_y):
 
+    cards_idx = np.where(np.logical_and(cards_y>=1, cards_y<=55))
+    cards_55 = cards_X[cards_idx]
+    
     h,w,z = cards_X[0,:,:,:].shape
-    #print(w,h)
     w11 = w * 11
     h5 = h * 5
     collage = np.zeros((h5,w11,3),np.uint8)
     idx = 0
     for r in range(0,5):
         for c in range(0,11):
-            collage[r*h:(r+1)*h,c*w:(c+1)*w,:] = cards_X[idx,:,:,:]
+            collage[r*h:(r+1)*h,c*w:(c+1)*w,:] = cards_55[idx,:,:,:]
             idx = idx + 1
 
+    title = "Deck %02d"%(deck_id)
     cv2.imshow(title,collage)
     cv2.waitKey(0)
     #imgplot = plt.imshow(cv2.cvtColor(collage,cv2.COLOR_BGR2RGB))
@@ -139,28 +122,22 @@ print("")
 print("VIEW CARD DECKS:")
 print("... press any key to continue ...")
 
-create_collage("Deck 01",train1_X,train1_y)
-create_collage("Deck 02",train2_X,train2_y)
-create_collage("Deck 03",train3_X,train3_y)
-create_collage("Deck 04",train4_X,train4_y)
-create_collage("Deck 05",train5_X,train5_y)
-create_collage("Deck 06",train6_X,train6_y)
-create_collage("Deck 07",train7_X,train7_y)
+for d in range(0,nb_card_decks):
+    create_collage(d+1,train_X[d],train_y[d])
 
 #
 # Display 57 cards
 #
 
-train_X = np.concatenate( (train1_X,train2_X,train3_X,train4_X,train5_X,train6_X,train7_X), axis=0 )
-train_y = np.concatenate( (train1_y,train2_y,train3_y,train4_y,train5_y,train6_y,train7_y), axis=0 )
+train_X = np.concatenate( train_X, axis=0 )
+train_y = np.concatenate( train_y, axis=0 )
 ntrain = len(train_y)
 
 print("")
 print("VIEW CARDS:")
 print("... press any key to continue (ESC to quit) ...")
 
-for i in range(1,57+1):
-#for i in range(1,5):
+for i in range(0,57+1):
     idx = (train_y==i) 
     cards_X = train_X[idx]
     cards_y = train_y[idx]
@@ -195,7 +172,6 @@ with open(dir+'/dobble_symbols.txt','r') as file:
             row[0] = '1'
         symbol_id = int(row[0])
         symbol_label = row[1]
-        #print(symbol_id,symbol_label)
         symbols[symbol_id] = symbol_label
 
 print("")
@@ -216,7 +192,6 @@ with open(dir+'/dobble_card_symbol_mapping.txt','r') as file:
         for i,val in enumerate(row[1:]):
             if val=='1':
                 card_mapping.append( i+1 )
-        #print(card_id,card_mapping)
         mapping[card_id] = card_mapping
 
 print("")
@@ -235,10 +210,8 @@ print("... press any key to continue (ESC to quit) ...")
 
 
 for i in range(100):
-#for i in range(10):
     idx1 = random.randrange(0,ntrain)
     idx2 = random.randrange(0,ntrain)
-    #print(idx1,idx2)
     card1_X = train_X[idx1,:,:,:]
     card1_y = train_y[idx1]
     card2_X = train_X[idx2,:,:,:]
